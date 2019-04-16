dcd7c0606507c087058f4c0fa9b0a220dd32e5b7
Tia96 <i.tai9996@hotmail.co.jp>
2019-04-17 08:49:08 +0900

    add drawShapes

--- main.elm
+++ main.elm
@@ -1,6 +1,8 @@
 import Browser
 import Html exposing (..)
-import Array
+import Array exposing (Array)
+import Canvas exposing (Shape, fill, shapes, rect)
+import Color
 
 main =
     Browser.element
@@ -37,24 +39,39 @@ type Move
 --    | RRotate
 --    | LRotate
 
-type alias Board = Array Array(CellState)
+type alias Board = Array (Array CellState)
 
-type alias Pos =  {x : Int, y : Int}
+type alias Pos = 
+    { x : Int
+    , y : Int
+    }
 
 type alias Model =
     { board : Board
     , nowMino : Mino
-    , minos : List (Mino)
+    , minos : List Mino
     }
 
 
 type Msg
     = Update
 
+
+initBoard : Board
+initBoard = 
+    Array.repeat 15 <| Array.fromList(1::2::3::[])
+
+
+init : (Model, Cmd Msg)
+init = 
+    ({ 
+
+    }, Cmd.none)
+
 minoPos : MinoType -> List(Pos)
 minoPos type_ =
     case type_ of
-        T -> [{x=0,y=0},{x=1,y=0},{x=2,y=0},{x=1,y=1}]
+        T -> [(0,0),(1,0),(2,0),(1,1)]
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -62,9 +79,9 @@ update msg model =
         Update ->
             (updateModel model, Cmd.none)
 
-replace : Pos -> Board -> Int
+replace : Pos -> Board -> Board
 replace {x, y} board =
-    Array.set y (Array.get y board |> Array.set x Block) board
+    Array.set y (Array.set x Block <| Array.get y board ) board
 
 putMinoToBoard : Board -> Mino -> Board
 putMinoToBoard board mino =
@@ -79,42 +96,46 @@ updateModel model =
        , minos = model.minos ++ model.nowMino
        }
     else
-        { model
-        | nowMino = 
-            { nowMino
-            | topPos =
-                { y = model.nowMino.topPosy + 1}
-            } 
-        }
+        { model | nowMino = moveMino model.nowMino Down }
 
-updateNowMino : Model -> Mino
-updateNowMino model =
-    if isCollide <| moveMino Down model.nowMino == True then
-        addBoard
+newMino : Mino
+newMino =
+    { topPos = {x = 3, y = 0}
+    , state = Moving
+    , type_ = T
+    }
 
-fixMino : Mino -> Board -> Board
-
-updateState : Pos -> CellState
+{--
+updateState : Point -> CellState
 updateState pos =
     let
         upState = getState
     in
-
-moveMino : Move -> Mino -> Mino
+--}
+moveMino : Mino -> Move -> Mino
 moveMino move mino =
     case move of
-        Down -> {mino | topPos = {mino.topPos.x, mino.topPos.y+1}}
-        Left -> {mino | topPos = {mino.topPos.x-1, mino.topPos.y}}
-        Right -> {mino | topPos = {mino.topPos.x+1, mino.topPos.y}}
+        Down -> {mino | topPos = down mino.topPos}
+        Left -> {mino | topPos = left mino.topPos}
+        Right -> {mino | topPos = right mino.topPos}
 
-getEachPos : Mino -> List(Pos)
+down : Pos -> Pos
+down {x, y} = {x = x, y = y+1}
+
+right : Pos -> Pos
+right {x, y} = {x = x+1, y = y}
+
+left : Pos -> Pos
+left {x, y} = {x = x-1, y = y}
+
+getEachPos : Mino -> List Pos
 getEachPos mino =
     List.map (+) mino.topPos (minoPos mino.type_)
 
 isCollide : Model -> Mino -> Bool
 isCollide model mino =
     List.map (\pos -> getState pos model) (getEachPos mino)
-    |> List.filter ((\=) Empty)
+    |> List.filter ((/=) Empty)
     |> List.length > 0
 
 getState : Pos -> Model -> CellState
@@ -122,19 +143,21 @@ getState pos model =
     Array.get pos.y model.board
     |> Array.get pos.x
 
-minoToCell : Pos -> Mino -> List(Pos)
+--minoToCell : Pos -> Mino -> List Pos
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "New Html Program" ]
+    Canvas.toHtml (500 , 500)
+        []
+        [ shapes [ fill Color.gray ] shapeBoard ]
+
+shapeBoard : Model -> List Shape
+shapeBoard model =
+    List.concatMap (\mino -> 
+        List.indexedMap (\pos -> rect (pos.x * 30, pos.y * 30) 30 30) getEachPos mino
+    )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
-
-init : (Model, Cmd Msg)
-init = 
-    (Model modelInitialValue, Cmd.none)
